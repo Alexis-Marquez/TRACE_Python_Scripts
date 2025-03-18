d094f1506a4d7a8919f3344d4b08a55bca534dd5
Alexis Marquez <alexismarq337@gmail.com>
2025-03-18 10:16:21 -0600

    Added crawler constructor, set config and reset methods, as well as unit tests

--- Tests/crawler_test.py
+++ Tests/crawler_test.py
@@ -7,6 +7,51 @@ def setUpCrawler():
     crawler = Crawler.Crawler()
     yield crawler
 
-def test_crawl(setUpCrawler):
-    result = setUpCrawler.crawl()
-    assert isinstance(result, dict)
+# def test_crawl_result(setUpCrawler):
+#     result = setUpCrawler.crawl()
+#     assert isinstance(result, dict)
+
+def test_crawler_constructor1(setUpCrawler):
+    assert setUpCrawler.getConfig() is not None
+
+def test_crawler_constructor2(setUpCrawler):
+    assert len(setUpCrawler.getConfig()) > 0
+
+def test_crawler_constructor3(setUpCrawler):
+    setUpCrawler.setConfig({"TargetURL": "www.google.com/",
+                "CrawlDepth": 100,
+                "PageNumberLimit": 200,
+                "UserAgent": "Mozilla/5.0",
+                "RequestDelay": 20000})
+
+    assert setUpCrawler.getConfig() == {"TargetURL": "www.google.com/",
+                "CrawlDepth": 100,
+                "PageNumberLimit": 200,
+                "UserAgent": "Mozilla/5.0",
+                "RequestDelay": 20000}
+
+def test_crawler_constructor4():
+    crawler = Crawler.Crawler({
+        "TargetURL": "",
+        "CrawlDepth": 10,
+        "PageNumberLimit": 20,
+        "UserAgent": "",
+        "RequestDelay": 2000
+    })
+    assert crawler.getConfig() == {
+        "TargetURL": "",
+        "CrawlDepth": 10,
+        "PageNumberLimit": 20,
+        "UserAgent": "",
+        "RequestDelay": 2000
+    }
+def test_crawler_reset():
+    crawler = Crawler.Crawler({
+            "TargetURL": "",
+            "CrawlDepth": 10,
+            "PageNumberLimit": 20,
+            "UserAgent": "",
+            "RequestDelay": 2000
+        })
+    crawler.reset()
+    assert len(crawler.getConfig()) > 0
--- backend/Crawler.py
+++ backend/Crawler.py
@@ -1,18 +1,34 @@
 class Crawler:
-    startingURL = ''
+    config = dict()
 
-    def __init__(self):
-        pass
+    def __init__(self, config = None):
+        if config is not None:
+            self.config = config
+        if config is None:
+            self.config = {
+                "TargetURL": "",
+                "CrawlDepth": 10,
+                "PageNumberLimit": 20,
+                "UserAgent": "",
+                "RequestDelay": 2000
+            }
     def crawl(self):
         pass
     def setStartingURL(self):
         pass
     def getConfig(self):
-        pass
+        return self.config
     def setConfig(self, config):
-        pass
+        if config is not None:
+            self.config = config
     def reset(self):
-        pass
+        self.config = {
+            "TargetURL": "",
+            "CrawlDepth": 10,
+            "PageNumberLimit": 20,
+            "UserAgent": "",
+            "RequestDelay": 2000
+        }
     def getCrawlResults(self):
         pass
     def getTree(self):
